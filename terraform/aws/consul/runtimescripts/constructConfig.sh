c9b683a2599503c9a4e22456811944f89d050f11
Surender Yeruva <suri.inc@gmail.com>
2019-03-28 13:43:59 -0400

    Commit Consul

---
+++ terraform/aws/consul/runtimescripts/constructConfig.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+sudo wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
+sudo chmod u+x ec2-metadata
+PRIVATE_IP=`ip route get 1 | awk '{print $NF;exit}'`
+EC2_INSTANCE_ID=`/var/tmp/consul/ec2-metadata | grep instance-id  | cut -d: -f2 | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//'`
+UITRUEORFALSE=$2
+CLUSTER_TAG_KEY=$3
+CLUSTER_TAG_VALUE=$4
+SERVERTRUEORFALSE=$5
+BOOTSTRAP_VALUE=$6
+
+sudo cat $1 | sed "s|@@PRIVATE_IP@@|${PRIVATE_IP}|g" | sed "s|@@BOOTSTRAP_VALUE@@|${BOOTSTRAP_VALUE}|g" | sed "s|@@SERVERTRUEORFALSE@@|${SERVERTRUEORFALSE}|g" | sed "s|@@UITRUEORFALSE@@|${UITRUEORFALSE}|g" |sed "s|@@EC2_INSTANCE_ID@@|${EC2_INSTANCE_ID}|g" | sed  "s|@@CLUSTER_TAG_KEY@@|${CLUSTER_TAG_KEY}|g" | sed "s|@@CLUSTER_TAG_VALUE@@|${CLUSTER_TAG_VALUE}|g" > /etc/consul/config/consul.json
