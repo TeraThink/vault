8171d75fd86e100ff5b2d301daaa0744db98762d
Surender Yeruva <suri.inc@gmail.com>
2019-03-27 20:08:28 -0400

    Commit 1

--- terraform/aws/consul/instances/root/install-run-consul-server-centos.sh
+++ terraform/aws/consul/instances/root/install-run-consul-server-centos.sh
@@ -5,7 +5,7 @@ echo "Passed Vars test 11:: "$cluster_tag_key
 echo "Passed Vars test 22:: "$cluster_tag_value
 
 echo "Passed Vars test 33:: "$1
-echo "Passed Vars test 44:: "$2
+echo "Passed Vars test 12 44:: "$2
 
 cluster_tag_key=$1
 cluster_tag_value=$2
