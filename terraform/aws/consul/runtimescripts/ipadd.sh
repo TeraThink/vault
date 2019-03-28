746b84ed615c04f3781aee1b0b452974536a50de
Surender Yeruva <suri.inc@gmail.com>
2019-03-28 07:16:44 -0400

    Commit 1

--- terraform/aws/consul/runtimescripts/ipadd.sh
+++ terraform/aws/consul/runtimescripts/ipadd.sh
@@ -4,9 +4,10 @@ sudo wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
 sudo chmod u+x ec2-metadata
 PRIVATE_IP=`ip route get 1 | awk '{print $NF;exit}'`
 EC2_INSTANCE_ID=`/var/tmp/consul/ec2-metadata | grep instance-id  | cut -d: -f2 | sed 's/^[[:blank:]]*//;s/[[:blank:]]*$//'`
-TRUEORFALSE=$2
+UITRUEORFALSE=$2
 CLUSTER_TAG_KEY=$3
 CLUSTER_TAG_VALUE=$4
+SERVERTRUEORFALSE=$5
 
 
-sudo cat $1 | sed "s|@@PRIVATE_IP@@|${PRIVATE_IP}|g" | sed "s|@@TRUEORFALSE@@|${TRUEORFALSE}|g" |sed "s|@@EC2_INSTANCE_ID@@|${EC2_INSTANCE_ID}|g" | sed  "s|@@CLUSTER_TAG_KEY@@|${CLUSTER_TAG_KEY}|g" | sed "s|@@CLUSTER_TAG_VALUE@@|${CLUSTER_TAG_VALUE}|g" > /etc/consul/consul.json
+sudo cat $1 | sed "s|@@PRIVATE_IP@@|${PRIVATE_IP}|g" | sed "s|@@SERVERTRUEORFALSE@@|${SERVERTRUEORFALSE}|g" | sed "s|@@UITRUEORFALSE@@|${UITRUEORFALSE}|g" |sed "s|@@EC2_INSTANCE_ID@@|${EC2_INSTANCE_ID}|g" | sed  "s|@@CLUSTER_TAG_KEY@@|${CLUSTER_TAG_KEY}|g" | sed "s|@@CLUSTER_TAG_VALUE@@|${CLUSTER_TAG_VALUE}|g" > /etc/consul/consul.json
